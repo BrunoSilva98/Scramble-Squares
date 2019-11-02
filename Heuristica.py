41cde65e11349554b64e834aa0bd0292890956ea
BrunoSilva98 <brunops98@hotmail.com>
2019-11-02 16:45:44 -0300

    Criacao da h1 (elementos fora de posicao)

--- Heuristica.py
+++ Heuristica.py
@@ -1,4 +1,19 @@
 from Scramble_Squares import Scramble_Squares
 
 class Heuristica:
-    
+    def __init__(self):
+        self.squares = Scramble_Squares()
+        self.elementosFora = list()
+        
+    def h1(self,no): #h1 será a busca por quantidade de elementos fora da posição
+        qtde_fora = 0
+        for linha in range(3):
+            for coluna in range(3):
+                if no.estado[linha][coluna] != self.squares.matrizObjetivo[linha][coluna]:
+                    qtde_fora += 1
+                    self.elementosFora.append(no.estado[linha][coluna])
+        return qtde_fora
+
+
+    def h2(self,no): #h2 será a distancia dos elementos até sua posição objetivo
+        pass
